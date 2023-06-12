9a63b33bc7c49c381575aed6d0a2b093758fb748
Jiraf400 <sparolem234@gmail.com>
2023-06-12 09:53:03 +0300

    added jwt converter

--- src/main/java/com/jirafik/security/SpringbootKeycloakApplication.java
+++ src/main/java/com/jirafik/security/SpringbootKeycloakApplication.java
@@ -5,9 +5,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringbootKeycloakApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SpringbootKeycloakApplication.class, args);
     }
-
 }
+
+/* TERMS
+
+Realm - security domain that manages a group of users, their creds, roles
+
+Clients - applications that interacted with keycloak security
+
+Client Scopes - permissions and claims of roles
+
+Users - customers
+
+Groups - represent a way to manage common attributes and roles mapping for a set of users
+
+ */
