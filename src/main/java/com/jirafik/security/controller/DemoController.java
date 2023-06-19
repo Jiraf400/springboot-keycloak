4902d4cdd18d5a0fb85e54f98e60ec89fea0e8fb
Jiraf400 <sparolem234@gmail.com>
2023-06-19 09:27:17 +0300

    initial

--- src/main/java/com/jirafik/security/controller/DemoController.java
+++ src/main/java/com/jirafik/security/controller/DemoController.java
@@ -1,20 +1,19 @@
 package com.jirafik.security.controller;
 
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 @RestController
 @RequestMapping("/api/demo")
 public class DemoController {
 
-    @GetMapping("/hello")
-    public String getHello() {
-        UserDetails jwt = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    @GetMapping("/secured/hello")
+    public String getHello(Principal principal) {
 
-        return "<h1> Hello " +jwt.getUsername() + " from secured method </h1>";
+        return "<h1> Hello " + principal.getName() + " from secured method </h1>";
     }
 
     @GetMapping("/public/hello")
